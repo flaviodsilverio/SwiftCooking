5dd456fb12ccab347fb5622feac04891870754c6
flaviodsilverio <flaviosilverio@MacBook-Pro-de-Flavio.local>
2015-04-25 22:42:09 +0100

    some changes on shopping cart

--- SwiftCooking/SwiftCooking/ShoppingCart.swift
+++ SwiftCooking/SwiftCooking/ShoppingCart.swift
@@ -8,22 +8,34 @@
 
 import UIKit
 
+
+
 class ShoppingCart: UITableViewController {
+
+    var data : Dictionary <String,[String]>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
-
+       self.getData()
         
+        data["Batatas com ovos"]=["batatas", "ovos"]
+        data["pao com fiambre"]=["pao","fiambre","manteiga"]
+        data["Misc"]=["sdf"]
+
+
        // let item : UIBarButtonItem
         //item.title="+"
-        let navigationItem = UIBarButtonItem()
-
-        navigationItem.title="+"
+       // let navigationItem = UIBarButtonItem()
+        NSLog("%@",data.keys.array)
+        //navigationItem.title="+"
+        let navigationItemEdit = UIBarButtonItem()
+        navigationItemEdit.title = "Edit"
         
         
-        self.navigationItem.rightBarButtonItem=navigationItem
+        
+        
+        self.navigationItem.rightBarButtonItem=navigationItemEdit
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -41,42 +53,58 @@ class ShoppingCart: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 4
+        return 1 + data.count
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 3
+
+        if(section==0){
+            return 1
+        } else{
+            return data[data.keys.array[section-1]]!.count
+        }
+        
     }
     
     
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch(section){
-            
-        case 0:
-            return "Starters"
-        case 1:
-            return "Mains"
-        case 2:
-            return "Desserts"
-        case 3:
-            return "Drinks"
-            
-        default:
-            return ""
+        
+        if(section==0){
+            return "Add Item"
+        } else {
+        
+            return data.keys.array[section-1]
         }
-        
-        
         
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
         
+        switch(indexPath.section){
         
-        cell.textLabel?.text = "This is something"
-        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        case 0:
+            
+            cell.textLabel?.text = "+"
+            cell.textLabel?.textAlignment=NSTextAlignment.Center
+            cell.textLabel?.textColor = UIColor.whiteColor()
+            cell.selectionStyle = UITableViewCellSelectionStyle.None
+            cell.backgroundColor = UIColor.grayColor()
+            
+            break;
+            
+            
+        default:
+            
+            cell.textLabel?.text = "This is something"
+            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+            
+            break;
+        
+        }
+        
+
         // Configure the cell...
         
         return cell
@@ -127,4 +155,15 @@ class ShoppingCart: UITableViewController {
     }
     */
 
+    // MARK: - getting the data
+    func getData(){
+    
+//        var defaults = NSUserDefaults.standardUserDefaults()
+//        
+//        if((defaults.valueForKey("shoppingCartData")) != nil){
+//            data = defaults.valueForKey("shoppingCartData")?.dictionaryRepresentation()
+//        } else {
+            data = Dictionary<String,[String]>()
+//        }
+    }
 }
